f0e8b9081cb513d28260f24f04c2ad6093a9bb5f
Trevbo25 <trevorbrown25@gmail.com>
2017-11-28 10:22:47 -0700

    create server and set up very basic sockets.io

---
+++ server/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      controller = require('./controller'),
+      massive = require('massive'),
+      session = require('express-session'),
+      Auth0Strategy = require('passport-auth0'),
+      axios = require('axios'),
+      cors = require('cors');
+
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server)
+app.use(cors());
+app.use(bodyParser.json());
+
+// massive(process.env.CONNECTION_STRING).then( db => {
+//         app.set('db', db);
+// }).catch('err', err => console.log(err))
+
+app.get('/', function(req, res){
+    res.send('<h1>Hello world!</h1>')
+})
+
+const PORT = 3212;
+server.listen(PORT, () => console.log("I'm listenin' brotha' on port ", PORT));
+
+io.on('connection', function(socket){
+    console.log('we are connected');  
+})
